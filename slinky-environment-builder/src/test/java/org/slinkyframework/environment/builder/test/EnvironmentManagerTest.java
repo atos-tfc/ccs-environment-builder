07a6ca599d51d194bcb825ccef70df71134fe541
Keith Lyall <keith.lyall@gmail.com>
2018-09-14 18:15:29 +0100

    Use updated Mockito

--- slinky-environment-builder/src/test/java/org/slinkyframework/environment/builder/test/EnvironmentManagerTest.java
+++ slinky-environment-builder/src/test/java/org/slinkyframework/environment/builder/test/EnvironmentManagerTest.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.slinkyframework.environment.builder.definition.BuildDefinition;
 import org.slinkyframework.environment.builder.definition.BuildDefinitionCollector;
 import org.slinkyframework.environment.builder.definition.BuildPriority;
